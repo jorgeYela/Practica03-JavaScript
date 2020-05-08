a153e9ad91401e2f976b8ff29795af663ae36d16
jorgeYela <yelajorge6@gmail.com>
2020-05-08 16:23:21 -0600

    Programacion Boton 1

--- Galeria/js/funcionamiento.js
+++ Galeria/js/funcionamiento.js
@@ -1,23 +1,109 @@
-var indice = 0;
-imagenesHabilitadas = new Array();
-imagenesTotal = new Array();
-imagenesTotal[0] = '<img src="/Galeria/Imagenes/bandera.jpg" width="780" height="400"/>';
-imagenesTotal[1] = '<img src="/Galeria/Imagenes/bitri.png" width="780" height="400"/>';
-imagenesTotal[2] = '<img src="/Galeria/Imagenes/ElMasGrande.jpg" width="780" height="400"/>';
-imagenesTotal[3] = '<img src="/Galeria/Imagenes/elnacional13estrellas.jpg" width="780" height="400"/>';
-imagenesTotal[4] = '<img src="/Galeria/Imagenes/ElNacional2019.jpg" width="780" height="400"/>';
-imagenesTotal[5] = '<img src="/Galeria/Imagenes/ElNacionalCD.jpg" width="780" height="400"/>';
-imagenesTotal[6] = '<img src="/Galeria/Imagenes/indumentaria.jpg" width="780" height="400"/>';
-imagenesTotal[7] = '<img src="/Galeria/Imagenes/MareaRoja.jpg" width="780" height="400"/>';
-imagenesTotal[8] = '<img src="/Galeria/Imagenes/NachoTuPapa.jpg" width="780" height="400"/>';
-imagenesTotal[9] = '<img src="/Galeria/Imagenes/NachoTuPapaHinchada.jpg" width="780" height="400"/>';
-indice = Math.random() * (imagenesTotal.length);
-indice = Math.floor(indice);
-for (i=0;i<=4;i++) {
-    if (indice == imagenesTotal.length) indice = 0;
-    imagenesHabilitadas[i] = imagenesTotal[indice];
-    indice++;
+function generar() {
+
+    var indice = 0;
+
+    imagenesHabilitadas = new Array();
+    imagenesTotal = new Array();
+
+    //TODAS LAS IMAGENES DENTRO DEL VECTOR imagenesTotal. 
+    imagenesTotal[0] = '/Galeria/Imagenes/bandera.jpg';
+    imagenesTotal[1] = '/Galeria/Imagenes/bitri.png';
+    imagenesTotal[2] = '/Galeria/Imagenes/ElMasGrande.jpg';
+    imagenesTotal[3] = '/Galeria/Imagenes/elnacional13estrellas.jpg';
+    imagenesTotal[4] = '/Galeria/Imagenes/ElNacional2019.jpg';
+    imagenesTotal[5] = '/Galeria/Imagenes/ElNacionalCD.jpg';
+    imagenesTotal[6] = '/Galeria/Imagenes/indumentaria.jpg';
+    imagenesTotal[7] = '/Galeria/Imagenes/MareaRoja.jpg';
+    imagenesTotal[8] = '/Galeria/Imagenes/NachoTuPapa.jpg';
+    imagenesTotal[9] = '/Galeria/Imagenes/NachoTuPapaHinchada.jpg';
+
+    //GENERACION DE NUMEROS ALEATORIOS. 
+    indice = Math.random() * (imagenesTotal.length);
+    indice = Math.floor(indice);
+    
+    for (i=0;i<=4;i++) {
+        if (indice == imagenesTotal.length) indice = 0;
+        imagenesHabilitadas[i] = imagenesTotal[indice];
+        indice++;
+    }
+
+    /*document.getElementById('imagen0').src = imagenesHabilitadas[0];
+    document.getElementById('imagen1').src = imagenesHabilitadas[1];
+    document.getElementById('imagen2').src = imagenesHabilitadas[2];
+    document.getElementById('imagen3').src = imagenesHabilitadas[3];
+    document.getElementById('imagen4').src = imagenesHabilitadas[4];*/
+
 }
-for(i=0;i<=imagenesHabilitadas.length;i++){
-   document.write(imagenesHabilitadas[i]);
+
+/*function carrousel(direccion){
+
+    contador = 0;
+
+    let direccion1 = direccion;
+
+    if(direccion1=='atras'){
+        if(contador==0){
+            document.getElementById('imagen').src= imagenesHabilitadas[imagenesHabilitadas.length-1];
+            contador++;
+        }else if (contador<imagenesHabilitadas.length-1) {
+            document.getElementById('imagen').src= imagenesHabilitadas[imagenesHabilitadas.length-1-contador];
+            contador++;
+        }else {
+            document.getElementById('imagen').src= imagenesHabilitadas[0];
+            //boton.botonAtras.disabled=true;
+            contador= imagenesHabilitadas.length -1;
+        }
+    }
+
+    if(direccion1=='adelante'){
+        if(contador==0){
+            document.getElementById('imagen').src= imagenesHabilitadas[0];
+            contador++;
+        }else if (contador<imagenesHabilitadas.length-1) {
+            document.getElementById('imagen').src= imagenesHabilitadas[contador];
+            contador++;
+        }else {
+            document.getElementById('imagen').src= imagenesHabilitadas[imagenesHabilitadas.length-1];
+            contador=0;
+        }
+    }
+
+}*/
+
+
+var contador=0;
+
+function moverDerecha(boton){
+    
+    if(contador == 0) {
+        document.getElementById('imagen').src = imagenesHabilitadas[0];
+        contador++;
+        var elemento = document.getElementById("texto");
+        elemento.innerHTML += contador+" desde if contador == 0------------------";
+    } else if(contador == 1) {
+        document.getElementById('imagen').src = imagenesHabilitadas[1];
+        contador++;
+        var elemento = document.getElementById("texto");
+        elemento.innerHTML += contador+" desde if contador == 1------------------";
+    } else if(contador == 2) {
+        document.getElementById('imagen').src = imagenesHabilitadas[2];
+        contador++;
+        var elemento = document.getElementById("texto");
+        elemento.innerHTML += contador+" desde if contador == 2-------------------";
+    } else if(contador == 3) {
+        document.getElementById('imagen').src = imagenesHabilitadas[3];
+        contador++;
+        var elemento = document.getElementById("texto");
+        elemento.innerHTML += contador+" desde if contador == 3--------------------";
+    } else if(contador == 4) {
+        document.getElementById('imagen').src = imagenesHabilitadas[4];
+        contador++;
+        var elemento = document.getElementById("texto");
+        elemento.innerHTML += contador+" desde if contador == 4--------------------";
+    } else {
+        boton.derechita.disable=true;
+        var elemento = document.getElementById("texto");
+        elemento.innerHTML += contador+" nada massssss serpiente soy perrin--------------------";
+    }
+
 }
