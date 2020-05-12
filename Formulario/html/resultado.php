58f43107430603760393dca79366182eea81be33
jorgeYela <yelajorge6@gmail.com>
2020-05-12 17:34:01 -0600

    Upgrated Formulario

---
+++ Formulario/html/resultado.php
@@ -0,0 +1,12 @@
+<?php
+
+$c = $_POST["cedula"];
+$n = $_POST["nombres"];
+
+echo "Su nombre es: " $n " y tengo el DNI: " $c ", HELLOW WORLD";
+
+?>
+<!DOCTYPE html>
+<html>
+    <body></body>
+</html>
